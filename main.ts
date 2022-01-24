7d0167904a9b30a1866fdcd4e0f0a8878aa4444b
Vitaly Ankudinov <vitaly.ankudinov@avast.com>
2022-01-25 03:16:56 +0700

    task

--- main.ts
+++ main.ts
@@ -1,13 +1,24 @@
 // возвращает сумму элементов массива
 export function arraySum(arr: number[]): number {
+  return arr.reduce((sum, num) => sum + num);
 }
 
 // возвращает исходную строку в Title Case
-export function toTitleCase(str: string[]): string {
+export function toTitleCase(str: string): string {
+  return str.replace(/^[A-Za-z]|\s[A-Za-z]/g, (match) => match.toUpperCase());
 }
 
 // возвращает Promise, который зарезолвит true, если b больше чем a, и зареджектит, если наоборот
 export function promiseCompare(a: number, b: number): Promise<true> {
+  const promise: Promise<true> = new Promise((resolve, reject)  => {
+    if (b > a)  {
+        resolve(true);
+    }
+
+      reject(new Error("error"));
+  });
+
+  return promise;
 }
 
 export async function asyncCompare(): Promise<true> {
@@ -15,14 +26,14 @@ export async function asyncCompare(): Promise<true> {
   let b: number = 5;
   let res: any = false;
 
-  while (res !== true) {
+  do {
     try {
-      res = promiseCompare(a, b);
+      res = await promiseCompare(a, b);
     }
     catch (e) {
-      b--;
+      b++;
     }
-  }
+  } while (res !== true)
 
   return res;
 }
